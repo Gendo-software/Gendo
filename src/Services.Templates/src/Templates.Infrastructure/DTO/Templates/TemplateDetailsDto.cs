1f597bbb2714f06ffd3b10e9e8a9bde59ac56381
Paweł Konieczny <koniecznypaul@gmail.com>
2019-08-26 21:48:53 +0200

    WebApi change: extend template details dto

--- src/Services.Templates/src/Templates.Infrastructure/DTO/Templates/TemplateDetailsDto.cs
+++ src/Services.Templates/src/Templates.Infrastructure/DTO/Templates/TemplateDetailsDto.cs
@@ -5,6 +5,11 @@ namespace Templates.Infrastructure.DTO.Templates
 {
     public class TemplateDetailsDto
     {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public bool DuringEditing { get; set; }
+        public DateTime CreatedAt { get; set; }
         public JObject Content { get; set; }
     }
 }
